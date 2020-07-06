a503bd12c8b45ef7e7118e4bd408f23093db4e90
k018c1081@it-neec.jp <50689332+k018c1081@users.noreply.github.com>
2020-07-06 13:49:26 +0900

    first commit

--- kadai02.py
+++ kadai02.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def top():
-    return "トップページ"
+    current_time = datetime.datetime.now()
+    return current_time.strftime('%m/%d %H:%M')
 
 if __name__ == "__main__":
     app.run(debug=True)
